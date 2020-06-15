5b2178f00500ce22a0f8f3e60cf8a5d92a9fe70f
Andrew Tropin <andrew@trop.in>
2020-06-15 17:16:47 +0300

    Add get_person_name fn instead of inline implementation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 
-person = sys.argv[1] if (len(sys.argv) > 1) else "world"
 def get_hello_str(lang):
     if lang == "ru":
         return "Привет"
@@ -10,6 +9,13 @@ def get_hello_str(lang):
         return "Hello"
 
 
+def get_person_name(args):
+    if len(args) > 1:
+        return args[1]
+    else:
+        return "world"
+
+person = get_person_name(sys.argv)
 lang = sys.argv[2] if (len(sys.argv) > 2) else "en"
 
 hello_str = get_hello_str(lang) + ","
