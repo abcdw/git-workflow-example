ed1c7d2a6855e361630356cc0bdbd995b0046a77
Andrew Tropin <andrew@trop.in>
2020-06-15 17:15:35 +0300

    Add get_hello_str function
    Which use lang for determining of hello str
    More explanantion here if it necessary

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import sys
 
 person = sys.argv[1] if (len(sys.argv) > 1) else "world"
+def get_hello_str(lang):
+    if lang == "ru":
+        return "Привет"
+    elif lang == "fr":
+        return "Bonjour"
+    else:
+        return "Hello"
+
+
 lang = sys.argv[2] if (len(sys.argv) > 2) else "en"
-hello_str = "hello,"
+
+hello_str = get_hello_str(lang) + ","
 
 print(hello_str, person)
