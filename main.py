cdc11fae8b45f6cbbf8e775b1362cb39325ddc3a
Andrew Tropin <andrew@trop.in>
2020-06-16 18:45:55 +0300

    Add person name argument for our cli

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import sys
 
-person = sys.argv[1] if (len(sys.argv) > 1) else "world"
 lang = sys.argv[2] if (len(sys.argv) > 2) else "en"
 hello_str = "hello,"
 
+def get_person_name(args):
+    if len(args) > 1:
+        return args[1]
+    else:
+        return "world"
+
+person = get_person_name(sys.argv)
 print(hello_str, person)
